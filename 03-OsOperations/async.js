550daca1c61c9200cb7e894e30c3bf0b900bbe66
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2023-03-21 23:07:53 -0300

    Async js example

--- 03-OsOperations/async.js
+++ 03-OsOperations/async.js
@@ -1,12 +1,23 @@
 const https = require('https')
+const http = require('http')
+const { Stream } = require('stream')
 
 const start = Date.now()
 
-https
-    .request('https://www.google.com/', res => {
-        res.on('data', () =>{})
-        res.on('end', () => {
-            console.log(Date.now() - start)
+function doRequest(){
+    http
+        .request('http://www.google.com/', res => {
+            res.on('data', (obj) =>{})
+            res.on('end', () => {
+                console.log(Date.now() - start)
+            })
         })
-    })
-    .end()
+        .end()
+}
+
+doRequest()
+doRequest()
+doRequest()
+doRequest()
+doRequest()
+doRequest()
