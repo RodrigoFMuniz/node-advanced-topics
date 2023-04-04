0793c4ad84cd55484ca4d5ce8b37a2245594bd25
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2023-04-03 23:12:20 -0300

    Cluster in node js

--- 05-cluster/server.js
+++ 05-cluster/server.js
@@ -1,14 +1,32 @@
-const express = require('express')
-const server =  express()
+const cluster = require('cluster')
 
-function doWork(duration){
-    const start = Date.now()
-    while(Date.now() - start < duration){}
+// Is the file being executed in master mode?
+if(cluster.isMaster){
+
+    //Cause index.js to be executed again, but in child mode
+    cluster.fork()
+    cluster.fork()
+    cluster.fork()
+    cluster.fork()
+
+}else{
+    // Im a child, Im going to act like a server and go notihing else
+    const express = require('express')
+    const server =  express()
+
+    function doWork(duration){
+        const start = Date.now()
+        while(Date.now() - start < duration){}
+    }
+
+    server.get('/', (req, res)=>{
+        doWork(5000)
+        res.status(200).send('Hi There')
+    })
+
+    server.get('/fast', (req, res)=>{
+        res.status(200).send('Faster many Forks')
+    })
+
+    server.listen(3000, ()=> console.log('Listening ... '))
 }
-
-server.get('/', (req, res)=>{
-    doWork(5000)
-    res.status(200).send('Hi There')
-})
-
-server.listen(3000, ()=> console.log('Listening ... '))
